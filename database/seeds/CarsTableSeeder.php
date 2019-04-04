13857424f0da683da85495f5389053c6fc810403
Toshimichi Mimoto <mimoto@w-tokyodo.com>
2019-04-04 17:26:53 +0900

    データベース開発に備えて Seeder を更新。

--- database/seeds/CarsTableSeeder.php
+++ database/seeds/CarsTableSeeder.php
@@ -14,27 +14,14 @@ class CarsTableSeeder extends Seeder
     public function run()
     {
         $cars = [
-            [
-                'vendor_id' => 1,
-                'name' => 'トウキョウドゥベンツ',
-                'vin' => '品川88つ555',
-            ], [
-                'vendor_id' => 1,
-                'name' => 'トウキョウドゥグラマン',
-                'vin' => '品川88そ7246',
-            ], [
-                'vendor_id' => 1,
-                'name' => 'トウキョウドゥグラマン搬入車',
-                'vin' => '品川100す251',
-            ], [
-                'vendor_id' => 1,
-                'name' => 'ドック車',
-                'vin' => '品川800さ3127',
-            ], [
-                'vendor_id' => 4,
-                'name' => 'ボナペティ',
-                'vin' => '練馬800す2669',
-            ],
+            ['vendor_id' => 1,  'status' => 1, 'created_at' => '2019-04-03 17:42:36', 'vin' => '品川88つ555', 'name' => 'トウキョウドゥベンツ',],
+            ['vendor_id' => 1,  'status' => 1, 'created_at' => '2019-04-03 17:42:36', 'vin' => '品川88そ7246', 'name' => 'トウキョウドゥグラマン',],
+            ['vendor_id' => 1,  'status' => 0, 'created_at' => '2019-04-03 17:42:36', 'vin' => '品川100す251', 'name' => 'トウキョウドゥグラマン搬入車',],
+            ['vendor_id' => 1,  'status' => 0, 'created_at' => '2019-04-03 17:42:36', 'vin' => '品川800さ3127', 'name' => 'ドック車',],
+            ['vendor_id' => 4,  'status' => 0, 'created_at' => '2019-04-03 17:42:36', 'vin' => '練馬800す2669', 'name' => 'ボナペティ',],
+            ['vendor_id' => 2,  'status' => 0, 'created_at' => '2019-04-04 12:25:56', 'vin' => '練馬800す2388', 'name' => 'elephant box',],
+            ['vendor_id' => 1,  'status' => 0, 'created_at' => '2019-04-04 12:58:24', 'vin' => '品川880あ740', 'name' => 'ネオ2号',],
+            ['vendor_id' => 14, 'status' => 0, 'created_at' => '2019-04-04 13:20:33', 'vin' => '品川830さ1966', 'name' => 'パラダイス',],
         ];
 
         foreach ($cars as $car) {
@@ -43,7 +30,7 @@ class CarsTableSeeder extends Seeder
                 'vendor_id' => $vendor_id,
                 'name' => $name,
                 'vin' => $vin,
-                'created_at' => Carbon::now(),
+                'created_at' => $created_at,
                 'updated_at' => Carbon::now(),
             ]);
         }
--- database/seeds/VendorsTableSeeder.php
+++ database/seeds/VendorsTableSeeder.php
@@ -14,16 +14,26 @@ class VendorsTableSeeder extends Seeder
     public function run()
     {
         $vendors = [
-            ['name' => 'TokyoDo', 'status' => 1],
-            ['name' => 'elephant box', 'status' => 8],
-            ['name' => 'HANAカレー', 'status' => 9],
-            ['name' => 'ボナペティ', 'status' => 1],
+            ['status' => 1, 'created_at' => '2019-04-02 19:51:57', 'name' => 'トウキョウドゥ',], # 1
+            ['status' => 8, 'created_at' => '2019-04-02 19:51:57', 'name' => 'elephant box',], # 2
+            ['status' => 9, 'created_at' => '2019-04-02 19:51:57', 'name' => 'HANAカレー',], # 3
+            ['status' => 1, 'created_at' => '2019-04-02 19:51:57', 'name' => 'ボナペティ',], # 4
+            ['status' => 9, 'created_at' => '2019-04-03 12:14:15', 'name' => '朔（ｓａｋｕ）',], # 5
+            ['status' => 9, 'created_at' => '2019-04-03 12:35:15', 'name' => 'ランチ・ランチ',], # 6
+            ['status' => 9, 'created_at' => '2019-04-03 15:22:59', 'name' => 'イーストダイナー',], # 7
+            ['status' => 8, 'created_at' => '2019-04-03 15:30:26', 'name' => 'タコデリオ',], # 8
+            ['status' => 9, 'created_at' => '2019-04-03 15:42:56', 'name' => 'M\'s com',], # 9
+            ['status' => 9, 'created_at' => '2019-04-03 15:58:37', 'name' => 'スブラキハウス',], # 10
+            ['status' => 1, 'created_at' => '2019-04-03 16:01:13', 'name' => 'アジアンランチ',], # 11
+            ['status' => 9, 'created_at' => '2019-04-03 16:32:05', 'name' => 'YammY',], # 12
+            ['status' => 9, 'created_at' => '2019-04-04 13:19:36', 'name' => 'スープ ファクトリー',], # 13
+            ['status' => 1, 'created_at' => '2019-04-04 13:19:59', 'name' => 'パラダイス',], # 14
         ];
         foreach ($vendors as $vendor) {
             DB::table('vendors')->insert([
                 'name' => $vendor['name'],
                 'status' => $vendor['status'],
-                'created_at' => Carbon::now(),
+                'created_at' => $vendor['created_at'],
                 'updated_at' => Carbon::now(),
             ]);
         }
