7ef586d46b35761044e532c34d0e15cf1a701ac1
Toshimichi Mimoto <mimoto@w-tokyodo.com>
2019-04-05 21:10:55 +0900

    データベースからSeeder を作ってくれるライブラリをインストール。
    `files` テーブルにカラムを追加するためデータベースのリフレッシュをしたかったから

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * https://github.com/orangehill/iseed
+         */
+        Orangehill\Iseed\IseedServiceProvider::class,
+
     ],
 
     /*
