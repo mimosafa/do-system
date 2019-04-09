d90f133d3a247904de9c7172b8874d4e413b4489
Toshimichi Mimoto <mimoto@w-tokyodo.com>
2019-04-09 20:07:10 +0900

    今後の開発に備えて事業者と車両の Seeder を生成

--- database/seeds/VendorsTableSeeder.php
+++ database/seeds/VendorsTableSeeder.php
@@ -12,12 +12,12 @@ class VendorsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('vendors')->delete();
-        
+
         \DB::table('vendors')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
                 'name' => 'トウキョウドゥ',
@@ -25,15 +25,15 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-02 10:51:57',
                 'updated_at' => '2019-04-04 09:20:16',
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 2,
                 'name' => 'elephant box',
                 'status' => 8,
                 'created_at' => '2019-04-02 10:51:57',
-                'updated_at' => '2019-04-02 10:51:57',
+                'updated_at' => '2019-04-09 18:23:52',
             ),
-            2 => 
+            2 =>
             array (
                 'id' => 3,
                 'name' => 'HANAカレー',
@@ -41,7 +41,7 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-02 10:51:57',
                 'updated_at' => '2019-04-03 10:39:09',
             ),
-            3 => 
+            3 =>
             array (
                 'id' => 4,
                 'name' => 'ボナペティ',
@@ -49,7 +49,7 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-02 10:51:57',
                 'updated_at' => '2019-04-02 10:51:57',
             ),
-            4 => 
+            4 =>
             array (
                 'id' => 5,
                 'name' => '朔（ｓａｋｕ）',
@@ -57,7 +57,7 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-03 03:14:15',
                 'updated_at' => '2019-04-03 06:15:24',
             ),
-            5 => 
+            5 =>
             array (
                 'id' => 6,
                 'name' => 'ランチ・ランチ',
@@ -65,7 +65,7 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-03 03:35:15',
                 'updated_at' => '2019-04-03 06:15:00',
             ),
-            6 => 
+            6 =>
             array (
                 'id' => 7,
                 'name' => 'イーストダイナー',
@@ -73,7 +73,7 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-03 06:22:59',
                 'updated_at' => '2019-04-03 06:23:07',
             ),
-            7 => 
+            7 =>
             array (
                 'id' => 8,
                 'name' => 'タコデリオ',
@@ -81,7 +81,7 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-03 06:30:26',
                 'updated_at' => '2019-04-03 06:30:32',
             ),
-            8 => 
+            8 =>
             array (
                 'id' => 9,
                 'name' => 'M\'s com',
@@ -89,15 +89,15 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-03 06:42:56',
                 'updated_at' => '2019-04-03 06:43:21',
             ),
-            9 => 
+            9 =>
             array (
                 'id' => 10,
                 'name' => 'スブラキハウス',
                 'status' => 9,
                 'created_at' => '2019-04-03 06:58:37',
-                'updated_at' => '2019-04-03 06:59:48',
+                'updated_at' => '2019-04-09 19:11:11',
             ),
-            10 => 
+            10 =>
             array (
                 'id' => 11,
                 'name' => 'アジアンランチ',
@@ -105,7 +105,7 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-03 07:01:13',
                 'updated_at' => '2019-04-03 07:01:37',
             ),
-            11 => 
+            11 =>
             array (
                 'id' => 12,
                 'name' => 'YammY',
@@ -113,7 +113,7 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-03 07:32:05',
                 'updated_at' => '2019-04-04 03:48:15',
             ),
-            12 => 
+            12 =>
             array (
                 'id' => 13,
                 'name' => 'スープ ファクトリー',
@@ -121,7 +121,7 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-04 04:19:36',
                 'updated_at' => '2019-04-04 04:19:46',
             ),
-            13 => 
+            13 =>
             array (
                 'id' => 14,
                 'name' => 'パラダイス',
@@ -129,8 +129,40 @@ class VendorsTableSeeder extends Seeder
                 'created_at' => '2019-04-04 04:19:59',
                 'updated_at' => '2019-04-04 04:20:06',
             ),
+            14 =>
+            array (
+                'id' => 15,
+                'name' => 'ＴＲＡＦＦＩＣ　ＣＡＦＥ',
+                'status' => 9,
+                'created_at' => '2019-04-08 19:10:29',
+                'updated_at' => '2019-04-09 18:19:59',
+            ),
+            15 =>
+            array (
+                'id' => 16,
+                'name' => 'アレックスコーヒー',
+                'status' => 9,
+                'created_at' => '2019-04-09 10:32:44',
+                'updated_at' => '2019-04-09 18:22:10',
+            ),
+            16 =>
+            array (
+                'id' => 17,
+                'name' => 'ビーンズカート',
+                'status' => 9,
+                'created_at' => '2019-04-09 10:33:14',
+                'updated_at' => '2019-04-09 19:12:05',
+            ),
+            17 =>
+            array (
+                'id' => 18,
+                'name' => 'マハッタ',
+                'status' => 9,
+                'created_at' => '2019-04-09 10:33:29',
+                'updated_at' => '2019-04-09 19:12:21',
+            ),
         ));
-        
-        
+
+
     }
 }
