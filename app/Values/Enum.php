cfe8692061089e2378c6d4948801295464f1d8cb
Toshimichi Mimoto <mimoto@w-tokyodo.com>
2019-04-09 17:32:28 +0900

    状態を表すValueObject（...になってるかな？）を作成。
    参考にしたのは：
    https://qiita.com/nunulk/items/2366addc3aab59e6c81e
    インストールしたライブラリは：
    https://github.com/myclabs/php-enum
    上記を継承したクラスを作成した理由は、その状態オブジェクトが何であるかを判定するマジックメソッド `is{$key}` を実装したかったから。

---
+++ app/Values/Enum.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Values;
+
+use MyCLabs\Enum\Enum as PhpEnum;
+use Str;
+
+abstract class Enum extends PhpEnum
+{
+    /**
+     * `is$Key` メソッドで equals 判定
+     */
+    public static function __call($name, $arguments = [])
+    {
+        if (preg_match('/^is(.+)$/', $name, $match)) {
+            $key = strtoupper(Str::snake($match[1]));
+            if ($this->isValidKey($key)) {
+                return $this->equals(static::__callStatic($key, []));
+            }
+        }
+
+        throw new \BadMethodCallException("No method constant '$name' in class " . \get_called_class());
+    }
+}
