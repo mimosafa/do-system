42fac2772d9cdccf172b790f45da8e2db31f4c8f
Toshimichi Mimoto <mimoto@w-tokyodo.com>
2019-05-14 16:41:56 +0900

    Blade: box のcomponent 化

--- resources/views/adminWstd/modules/tabContents.blade.php
+++ resources/views/adminWstd/modules/tabContents.blade.php
@@ -2,25 +2,37 @@
 
     @var string $id Tab & tab contents ID
     @var array $tabs
-    @var array $contents
+    @var array[Wstd\View\Admin\ViewModelInterface] $contents
 
 --}}
 
-<div class="nav-tabs-custom" id="{{ $id }}">
-    <ul class="nav nav-tabs {{--nav-justified--}}">
-        @foreach ($tabs as $name => $label)
-            <li>
-                <a href="#{{ $name }}_{{ $id }}" data-toggle="tab">
-                    {!! $label !!}
-                </a>
-            </li>
-        @endforeach
-    </ul>
-    <div class="tab-content clearfix">
-        @foreach ($contents as $content)
-        <div class="tab-pane" id="{{ $content->id() }}_{{ $id }}">
-            @include($content->template(), $content)
+@if (count($contents) > 1)
+
+    <div class="nav-tabs-custom" id="{{ $id }}">
+        <ul class="nav nav-tabs {{--nav-justified--}}">
+            @foreach ($tabs as $name => $label)
+                <li>
+                    <a href="#{{ $name }}_{{ $id }}" data-toggle="tab">
+                        {!! $label !!}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+        <div class="tab-content clearfix">
+            @foreach ($contents as $content)
+            <div class="tab-pane" id="{{ $content->id() }}_{{ $id }}">
+                @include($content->template(), $content)
+            </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
-</div>
+
+@else
+
+    @component('adminWstd.modules.contentBox', [
+        'title' => $contents[0]->title(),
+    ])
+        @include($contents[0]->template(), $contents[0])
+    @endcomponent
+
+@endif
