8451eba7cd6c25937c366536400814496ed24bfd
Toshimichi Mimoto <mimoto@w-tokyodo.com>
2019-08-13 15:01:42 +0900

    営業区域一覧の表示順を設定

--- packages/Wstd/Infrastructure/Eloquents/BusinessArea.php
+++ packages/Wstd/Infrastructure/Eloquents/BusinessArea.php
@@ -2,9 +2,20 @@
 
 namespace Wstd\Infrastructure\Eloquents;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class BusinessArea extends Model
 {
-    //
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder
+                ->orderBy('prefecture_id', 'asc')
+                ->orderBy('city_id', 'asc')
+            ;
+        });
+    }
 }
