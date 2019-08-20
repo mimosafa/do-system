68c252d1462ffb4081a82c3f723794155b4dd270
Toshimichi Mimoto <mimoto@w-tokyodo.com>
2019-08-20 18:38:36 +0900

    フロントと admin で laravel-mix のエントリーポイントを分ける
    mix-manifest.json でどちらか片方のマッピングしかしてくれなかったので laravel-mix-merge-manifest をインストール
    https://laravel-mix.com/extensions/merge-manifest

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,17 @@
-@extends('adminlte::page')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Do System Guest</title>
+</head>
+<body>
+    <h1>Do System</h1>
+    <div id="app">
+        <example-component></example-component>
+    </div>
 
-@section('title', 'AdminLTE')
-
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
-
-@section('content')
-    <p>You are logged in!</p>
-@stop
+    <script src="{{ asset('/js/app.js') }}"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,33 +1,3 @@
-const mix = require('laravel-mix');
-const path = require('path');
-
-require('laravel-mix-eslint');
-require('laravel-mix-stylelint');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-/*
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
-*/
-
-const sassOptions = mix.inProduction() ? {outputStyle: 'compressed'} : {outputStyle: 'expanded'};
-const resolveModules = [path.resolve('./resources/wstd/js/modules'), 'node_modules'];
-
-mix.js('resources/wstd/js/admin.js', 'public/wstd/js/admin.js')
-  .sass('resources/wstd/sass/admin.scss', 'public/wstd/css/admin.css', sassOptions)
-  .webpackConfig({output: {publicPath: '/wstd/'}, resolve: {modules: resolveModules}})
-  .eslint()
-  .stylelint({configFile: './.stylelintrc.js', files: ['**/*.scss']})
-  .extract(['jquery', 'bootstrap', 'vue'])
-  .version()
-;
+if (process.env.section) {
+  require(`${__dirname}/webpack.mix.${process.env.section}.js`);
+}
